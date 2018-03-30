63f44c46cb3500d703ef8b85281f57cceb561e6b
Dom Goodwin <d0m182goodwin@gmail.com>
2018-03-30 17:10:38 +0100

    don

---
+++ app/bundles/db/db.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+type Db struct{}
+
+func (d *Db) List() {
+	// Initialize a session in us-west-2 that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials.
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-2")},
+	)
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	// Get the list of tables
+	result, err := svc.ListTables(&dynamodb.ListTablesInput{})
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Tables:")
+	fmt.Println("")
+
+	for _, n := range result.TableNames {
+		fmt.Println(*n)
+	}
+
+	fmt.Println("")
+	return "fin"
+}
--- main.go
+++ main.go
@@ -7,15 +7,19 @@ import (
 	"github.com/domgoodwin/go-api/app/bundles/devicebundle"
 
 	"github.com/gorilla/mux"
+
+	"github.com/domgoodwin/go-api/app/bundles/db"
 )
 
 func main() {
+	d := &db.Db{}
+	d.List()
 	// Controllers declaration
 	kc := &devicebundle.DeviceController{}
 	r := mux.NewRouter()
 	s := r.PathPrefix("/api/v1/").Subrouter()
 	// Routes handling
-	s.HandleFunc("/kitties", kc.Index).Methods("GET")
+	s.HandleFunc("/device", kc.Index).Methods("GET")
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
